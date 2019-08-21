d8df6611267d726f2b2adceed6908a797b4dbb09
Partha <partharecrkgp@gmail.com>
2019-08-21 20:40:01 +0530

    Initial Push

---
+++ src/main/java/com/example/gatewayserviceg8s/Application.java
@@ -0,0 +1,20 @@
+package com.example.gatewayserviceg8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+@EnableSwagger2
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
