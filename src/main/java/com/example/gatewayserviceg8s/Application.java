edb96fb65071f251ceae724aef641946e37ab1f6
Partha <partharecrkgp@gmail.com>
2019-08-22 10:57:34 +0530

    aaa

--- src/main/java/com/example/gatewayserviceg8s/Application.java
+++ src/main/java/com/example/gatewayserviceg8s/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -11,10 +13,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableDiscoveryClient
 @EnableZuulProxy
 @EnableSwagger2
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	@GetMapping("/")
+	public String health() {
+		return "Hello Gateway Home Page";
 	}
 
 }
